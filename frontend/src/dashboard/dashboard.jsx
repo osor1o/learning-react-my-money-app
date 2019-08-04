e0d4b630264596b137bc9b777710d2c8634c1dfa
Osorio Neto <osor1o@hotmail.com>
2019-08-03 22:23:25 -0300

    create container dashboard with redux

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
+import { getSummary } from './dashboardActions'
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import Row from '../common/layout/row'
 import ValueBox from '../common/widget/valueBox'
 
 class Dashboard extends Component {
+
+    componentWillMount() {
+        this.props.getSummary()
+    }
+
     render() {
+        const { credit, debt } = this.props.summary
         return (
             <div>
                 <ContentHeader title='Dashboard' small='Versão 1.0' />
@@ -14,13 +23,13 @@ class Dashboard extends Component {
                     <Row>
                         <ValueBox 
                             cols='12 4' color='green' icon='bank' 
-                            value='R$ 10' text='Total de Créditos' />
+                            value={`R$ ${credit}`} text='Total de Créditos' />
                         <ValueBox 
                             cols='12 4' color='red' icon='credit-card' 
-                            value='R$ 10' text='Total de Débitos' />
+                            value={`R$ ${debt}`} text='Total de Débitos' />
                         <ValueBox 
                             cols='12 4' color='blue' icon='money' 
-                            value='R$ 0' text='Valor Consolidado' />
+                            value={`R$ ${credit - debt}`} text='Valor Consolidado' />
                     </Row>
                 </Content>
             </div>
@@ -28,4 +37,6 @@ class Dashboard extends Component {
     }
 }
 
-export default Dashboard
+const mapStateToProps = state => ({summary: state.dashboard.summary})
+const mapDispatchToProps = dispatch => bindActionCreators({getSummary}, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { applyMiddleware, createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import promise from 'redux-promise'
 
 import App from './main/App';
+import reducers from './main/reducers'
 
-ReactDOM.render(<App />, document.getElementById("app"));
+const store = applyMiddleware(promise)(createStore)(reducers)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById('app'));
