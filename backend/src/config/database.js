86daee8b39ee7172acb1d821caae63e9c9161e99
Osorio Neto <osor1o@hotmail.com>
2019-08-28 10:01:17 -0300

    created auth in backend

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost:27019/mymoney'
-, { useMongoClient: true })
+module.exports = mongoose.connect('mongodb://localhost:27019/mymoney',
+{ useMongoClient: true })
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = 
